39210f1936b49ee2df0c30808c1111ca47d8c95f
Unit111 <unit_1@abv.bg>
2024-12-24 15:11:48 +0200

    add report options

---
+++ tests/example_test.py
@@ -0,0 +1,4 @@
+from playwright.sync_api import Page, expect
+def test_open_link(page: Page):
+    page.goto("https://playwright.dev")
+    expect(page.get_by_text("Playwright").first).to_be_visible()
